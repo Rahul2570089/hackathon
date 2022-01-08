4932813a768917626ae6ceb48210ee6c120628be
rahul <rs2570089@gmail.com>
2022-01-08 09:49:06 +0530

    Hackathon project

---
+++ lib/home.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'home2.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+  
+}
+
+    TextEditingController x = TextEditingController();
+    TextEditingController y = TextEditingController();
+
+class _HomeState extends State<Home> {
+
+  List<Data> e1 = [];
+  TooltipBehavior? _tooltip;
+
+  @override
+  void initState() {
+    _tooltip = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  List<Data> getdata() {
+    return e1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent[600],
+        appBar: AppBar(
+          elevation: 0.0,
+          title: const Text('Money Tracker'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent[400],
+        ),
+        body:
+         SingleChildScrollView(
+           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              child: SafeArea(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  child: Scaffold(
+                    body: SfCartesianChart(
+                      title: ChartTitle(text: "Preview"),
+                      tooltipBehavior: _tooltip,
+                      series: <ChartSeries>[
+                        LineSeries<Data,double>(
+                        dataSource: e1, 
+                        xValueMapper: (Data data,_) => data.time, 
+                        yValueMapper: (Data data,_) => data.expense,
+                        dataLabelSettings: const DataLabelSettings(isVisible: true)
+                        )
+                      ],
+                      primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+                      primaryYAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+                    ),
+                      ),
+                ),
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number, 
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+              controller: x,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.deepOrangeAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.deepOrangeAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                hintText: 'Enter the investment',
+                hintStyle: const TextStyle(color: Colors.grey),
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number, 
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+              controller: y,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.deepOrangeAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.deepOrangeAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(20)
+                  ),
+                hintText: 'Enter the month/year',
+                hintStyle: const TextStyle(color: Colors.grey),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonBar(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                            if(x.text.isNotEmpty && y.text.isNotEmpty){
+                              e1.add(Data(double.parse(x.text),double.parse(y.text)));
+                              x.clear();
+                              y.clear();
+                            }
+                        });
+                      },
+                      child: const Text('Add data'),
+                      )
+                  ],
+                ),  
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    var n = MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                        Home2(e1),
+                    );
+                    Navigator.of(context).push(n);
+                  },
+                  child: const Text('See Graph'),
+                  )
+              ],
+            ),
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    e1.removeLast();
+                  },
+                  child: const Text('Remove Last'),
+                  )
+              ],
+            ),
+              ],
+            ),
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    e1.clear();
+                  },
+                  child: const Text('Reset'),
+                  )
+              ],
+            ),
+           ]),
+         )
+         ),
+    );
+  }
+}
---
+++ lib/home2.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+
+class Home2 extends StatefulWidget {
+
+  final List<Data> e1;
+  Home2(this.e1);
+
+  @override
+  _Home2State createState() => _Home2State();
+}
+
+    TextEditingController x = TextEditingController();
+    TextEditingController y = TextEditingController();
+
+class _Home2State extends State<Home2> {
+    late List<Data> _data;
+    late TooltipBehavior _tooltip;
+    
+  
+    @override
+  void initState() {
+    _data = getdata();
+    _tooltip = TooltipBehavior(enable: true);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SfCartesianChart(
+              title: ChartTitle(text: "Graph"),
+              tooltipBehavior: _tooltip,
+              series: <ChartSeries>[
+                LineSeries<Data,double>(
+                dataSource: _data, 
+                xValueMapper: (Data data,_) => data.time, 
+                yValueMapper: (Data data,_) => data.expense,
+                dataLabelSettings: const DataLabelSettings(isVisible: true)
+                )
+              ],
+              primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+              primaryYAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+            ),
+        ),
+      )
+    );
+  }
+  List<Data> getdata() {
+    return widget.e1;
+  }
+}
+
+class Data {
+  Data (this.expense,this.time);
+  final double expense;
+  final double time;
+}
